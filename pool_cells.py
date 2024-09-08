24dd4610818b8cc5dc7524297ccd930a5c0d63f1
pacochow <paco.ckchow@gmail.com>
2024-09-08 19:51:55 -0400

    Update

--- pool_cells.py
+++ pool_cells.py
@@ -3,16 +3,18 @@ import numpy as np
 from tqdm import tqdm
 from src.figures import *
 
+# Specify data date
+data = "B-24-08-29-AM"
 
-data = "S-24-04-03 "
-
-total_frames = 900
+# Specify number of total frames
+total_frames = 600
 frame_start = 0
 cells = np.zeros((total_frames, 2220, 1680))
 for i in tqdm(range(total_frames)):
-    image = plt.imread(f"Data/{data}/RawData/{i:0>4}.tif")
+    # Read TIFF files
+    image = plt.imread(f"Data/{data}/RawData/T{i+frame_start:0>3}.tif")
     cells[i] = image
-
+ 
 _, M, N = cells.shape
 K = 4
 L = 4
@@ -20,8 +22,9 @@ L = 4
 MK = M // K
 NL = N // L
 pooled = cells[:, :MK*K, :NL*L].reshape(total_frames, MK, K, NL, L).mean(axis=(2, 4))
-np.save(f"Data/{data}/Analysed_data/pooled_cells.npy", pooled)
 
+# Save .npy file
+np.save(f"Data/{data}/Analysed_data/pooled_cells.npy", pooled)
 
 
 
